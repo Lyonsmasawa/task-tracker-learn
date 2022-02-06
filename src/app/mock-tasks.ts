493702c3a8503c0862a578ecef576bf72f91e596
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-06 02:15:27 -0800

    learn to export and import interfaces

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Tasks";
+
+export const TASKS: Task[] = [
+    {
+      id: 1,
+      text: 'Go shopping',
+      day: 'Feb 7th at 9:30am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Head to the gym',
+      day: 'Feb 7th at 10:30am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Hiking',
+      day: 'Feb 7th at 11:3am',
+      reminder: false,
+    },
+  ];
